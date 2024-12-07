d3b5d2fbffb3a45720cd992e28e1e469fec7114c
nisharga <kabirnisharga@gmail.com>
2024-12-07 23:29:27 +0600

    feat: onboaring screen

---
+++ user/app/(routes)/login/index.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function index() {
+  return <Text>Login</Text>;
+}
---
+++ user/app/(routes)/onboarding/index.tsx
@@ -0,0 +1,7 @@
+import { View, Text } from "react-native";
+import React from "react";
+import OnBoardingScreen from "@/screens/onboarding/onboarding.screen";
+
+export default function index() {
+  return <OnBoardingScreen />;
+}
